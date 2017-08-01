388dac127a037535e098a76d42a1cf8b208673c5
Robert Lowe <RobLowe1992@Yahoo.com>
2017-08-01 01:40:18 -0400

    New design

--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -1,34 +1,46 @@
-var projects = [{
-    "img":"/images/simon.png",
-    "url":"https://roblowe1992.github.io/simon/",
-    "title":"Simon",
-    "github": "https://github.com/RobLowe1992/simon"
-},{
-    "img":"/images/techCrunch_mock.png",
-    "url":"https://roblowe1992.github.io/techCrunch/",
-    "title":"TechCrunch Mock Site",
-    "github": "https://github.com/RobLowe1992/techCrunch"
-},{
-    "img":"/images/rqg.png",
-    "url":"https://roblowe1992.github.io/Random-Quote-Generator/",
-    "title":"Random Quote Generator",
-    "github": "https://github.com/RobLowe1992/Random-Quote-Generator"
-}];
-
-var $projects = $('#projects');
-
-
-for (var i = 0; i < projects.length; i++) {
-    $projects.append(`<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4"><a target="_blank" href="${projects[i].url}"><img src="${projects[i].img}" id="project-${[i+1]}" style="width: 350px; height: 300px" alt="Featured Image 1" class="img-rounded"></a><h2 class="proj-title">${projects[i].title}</h2><a class="orange" target="_blank" href="${projects[i].github}">${projects[i].github}</a></div>`);
-}
-
-var $thumbnailImage = $('header section:nth-of-type(2) img');
 $(function() {
-    setTimeout(function() {
+    var projects = [{
+        "img": "/images/simon.png",
+        "url": "https://roblowe1992.github.io/simon/",
+        "title": "Simon",
+        "github": "https://github.com/RobLowe1992/simon"
+    }, {
+        "img": "/images/techCrunch_mock.png",
+        "url": "https://roblowe1992.github.io/techCrunch/",
+        "title": "TechCrunch Mock Site",
+        "github": "https://github.com/RobLowe1992/techCrunch"
+    }, {
+        "img": "/images/rqg.png",
+        "url": "https://roblowe1992.github.io/Random-Quote-Generator/",
+        "title": "Random Quote Generator",
+        "github": "https://github.com/RobLowe1992/Random-Quote-Generator"
+    }];
+
+    var $projects = $('#projects');
+    var $topText = $('.top-text');
+
+
+    // for (var i = 0; i < projects.length; i++) {
+    //     $projects.append(`<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4"><a target="_blank" href="${projects[i].url}"><img src="${projects[i].img}" id="project-${[i + 1]}" style="width: 350px; height: 300px" alt="Featured Image 1" class="img-rounded"></a><h2 class="proj-title">${projects[i].title}</h2><a class="orange" target="_blank" href="${projects[i].github}">${projects[i].github}</a></div>`);
+    // }
+
+    var $thumbnailImage = $('header section:nth-of-type(2) img');
+    setTimeout(function () {
         $('.fly-in-text').removeClass('temp-hide');
+        setTimeout(function () {
+            $topText.css({
+                "border": "solid 1em white",
+                "padding": "4em 1em 1em 1em"
+            });
+        }, 2500)
     }, 500);
-
     $thumbnailImage.fadeIn(500);
-});
 
+    $('.slide').click(function(e){
+        e.preventDefault();
+        var linkedHref = $(this).attr('href');
+        console.log(linkedHref)
+        $('html,body').animate({scrollTop: $(linkedHref).offset().top}, 1000);
+    });
+});
 
